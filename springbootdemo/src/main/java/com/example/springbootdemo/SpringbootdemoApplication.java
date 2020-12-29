61178c5159447d9ea0d8b838145dfea808a57e6b
zhiqiang-chen <925492350@qq.com>
2020-12-29 14:02:05 +0800

    新建简单的springboot测试

---
+++ springbootdemo/src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootdemoApplication {
+
+    @RequestMapping("/")
+    public String frist(){
+        return "My frist index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+}
