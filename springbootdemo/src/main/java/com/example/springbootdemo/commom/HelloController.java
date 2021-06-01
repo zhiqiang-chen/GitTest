02dcb562c71656cae4f3b3100ffe048104151c82
zhiqiang-chen <925492350@qq.com>
2021-06-01 15:31:29 +0800

    添加注解

--- springbootdemo/src/main/java/com/example/springbootdemo/commom/HelloController.java
+++ springbootdemo/src/main/java/com/example/springbootdemo/commom/HelloController.java
@@ -2,8 +2,8 @@ package com.example.springbootdemo.commom;
 
 import com.example.springbootdemo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO
@@ -11,15 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author czq
  * @date 2021/2/18 11:47
  */
-@Controller
+@RestController
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @RequestMapping("/hello")
-    public void hello(){
-
+    public String hello(){
+        System.out.println("hello");
+        return "hello";
     }
 
 }
