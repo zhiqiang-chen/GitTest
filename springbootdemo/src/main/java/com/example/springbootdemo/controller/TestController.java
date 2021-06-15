3d34a2605d809d9028fff57987778b4e68edf219
zhiqiang-chen <925492350@qq.com>
2021-06-15 10:33:27 +0800

    添加封装的安装包，mybatisplus直接调用dao层方法使用

--- springbootdemo/src/main/java/com/example/springbootdemo/controller/TestController.java
+++ springbootdemo/src/main/java/com/example/springbootdemo/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.example.springbootdemo.controller;
 
+import com.bonc.ioc.common.util.AppReply;
 import com.example.springbootdemo.entity.UserInfoEntity;
 import com.example.springbootdemo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * TODO
@@ -19,8 +24,11 @@ public class TestController {
     private TestService testService;
 
     @RequestMapping("/getInfo")
-    public UserInfoEntity getInfo(String userId){
-        UserInfoEntity userInfoEntity = testService.getById(userId);
-        return userInfoEntity;
+    public AppReply getInfo(String userId){
+//        UserInfoEntity userInfoEntity = testService.getById(userId);
+        List<Map<String,Object>> list = testService.getById(userId);
+        Map<String,Object> map = new HashMap<>();
+        map.put("list",list);
+        return AppReply.success(map);
     }
 }
--- springbootdemo/src/main/java/com/example/springbootdemo/service/TestService.java
+++ springbootdemo/src/main/java/com/example/springbootdemo/service/TestService.java
@@ -1,12 +1,17 @@
 package com.example.springbootdemo.service;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.springbootdemo.dao.UserInfoDao;
 import com.example.springbootdemo.entity.UserInfoEntity;
+import com.mysql.cj.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * TODO
@@ -21,8 +26,11 @@ public class TestService {
     @Autowired
     private UserInfoDao userInfoDao;
 
-    public UserInfoEntity getById(String userId) {
+    public List<Map<String,Object>> getById(String userId) {
         UserInfoEntity userInfoEntity = userInfoDao.selectById(userId);
-        return userInfoEntity;
+        QueryWrapper<UserInfoEntity> wrapper  = new QueryWrapper<UserInfoEntity>();
+        wrapper.eq(!StringUtils.isNullOrEmpty(""),"name","小兰");
+        List<Map<String,Object>> a = userInfoDao.selectMaps(wrapper);
+        return a;
     }
 }
